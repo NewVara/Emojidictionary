d300c86ed11eb210cc1c5666e7854ce62107a072
Vimal Ranchhod <ranchhod.vimal@gmail.com>
2017-07-30 09:10:06 +0200

    our real first commit

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EmojiDict2
+//
+//  Created by Vimal Ranchhod on 2017/07/30.
+//  Copyright © 2017 Vimal Ranchhod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
